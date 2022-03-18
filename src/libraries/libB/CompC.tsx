3202dc377cc181876d00d8e8afb8b9bb5b0133c0
Bhadresh Desai <bdesai@temenos.com>
2022-03-18 07:36:51 +0000

    Fix console error

--- src/libraries/libB/CompC.tsx
+++ src/libraries/libB/CompC.tsx
@@ -2,12 +2,12 @@ import CompA from "@libA/CompA";
 import CompB from "@libA/CompB";
 const CompC = (): JSX.Element => (
   <>
-    <p>
+    <div>
       <div>Component C from LibB</div>
       <div>Using CompA and CompB from LibA</div>
       <CompA />
       <CompB />
-    </p>
+    </div>
   </>
 );
 export default CompC;
